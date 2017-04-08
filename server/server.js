e1e19ce890db38ee1eb25dd31fb94b1d3e2547b7
Unknown <Worldedit05@gmail.com>
2017-04-08 17:22:16 -0500

    Setting up initial files part 1

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import Express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import path from 'path';
+
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+
+const app = new Express();
+
+if (process.env.NODE_ENV === 'development') {
+  const compiler = webpack(config);
+  app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+  app.use(webpackHotMiddleware(compiler));
+}
