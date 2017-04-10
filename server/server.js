1d204c85e2434bd87341e47ce4e54f38911c3376
Unknown <Worldedit05@gmail.com>
2017-04-09 20:25:53 -0500

    Dev server is up and running

--- server/server.js
+++ server/server.js
@@ -4,9 +4,11 @@ import bodyParser from 'body-parser';
 import path from 'path';
 
 import webpack from 'webpack';
-import config from '../webpack.config.dev';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
+import config from '../webpack.config.dev';
+
+import serverConfig from './config';
 
 const app = new Express();
 
@@ -15,3 +17,15 @@ if (process.env.NODE_ENV === 'development') {
   app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
   app.use(webpackHotMiddleware(compiler));
 }
+
+app.use(bodyParser.json({ limit: '20mb' }));
+app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }));
+app.use(Express.static(path.resolve(__dirname, '..dist')));
+
+app.listen(serverConfig.port, (error) => {
+  if (!error) {
+    console.log(`Server is running on port: ${serverConfig.port}`); // eslint-disable-line
+  }
+});
+
+export default app;
