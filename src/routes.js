bd1cc863c3a2ce2d777ff9c057ab016340d2b0c8
Unknown <Worldedit05@gmail.com>
2017-04-17 19:21:41 -0500

    Fixed routing order

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import App from './components/App';
 import NotFound from './components/NotFound';
@@ -7,7 +7,7 @@ import NotFound from './components/NotFound';
 const Routes = () => (
     <Router>
       <div>
-        <Route component={NavBar} />
+        <Route />
         <Switch>
           <Route exact path="/" component={App} />
           <Route component={NotFound} />
