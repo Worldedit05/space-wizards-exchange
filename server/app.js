0977d116253b79ed897f30ebed9b4158ec5e8621
Unknown <Worldedit05@gmail.com>
2017-04-20 19:26:28 -0500

    Changed the morgan log to be clearer in the console window

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
+app.use(morgan('Server_log[morgan] - :remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
 
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
