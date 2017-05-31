faeef882e1893e27121a5f661f8402d8680fa731
Aaron Lee <Worldedit05@gmail.com>
2017-05-31 03:51:52 -0500

    Started a check for duplicate card ids

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -1,7 +1,37 @@
 const router = require('express').Router();
 const axios = require('axios');
+const deepEqual = require('deep-equal');
 
 const pool = require('../db.js');
+
+function getModifiedCards(responseData) {
+  let changedCards = [];
+  let numberOfChangedCards = 0;
+  pool.query('SELECT data AS dbCard FROM card', [], (err, results) => {
+    if (err) {
+      return console.error('error running get modified cards query');
+    }
+
+    if (results.rowCount === 0) {
+      changedCards = responseData;
+      return changedCards;
+    }
+
+    results.rows.forEach((row) => {
+      const apiCardObj = responseData.find((value) => {
+        return value.code === row.dbcard.code;
+      });
+
+      if (!deepEqual(apiCardObj, row.dbcard)) {
+        changedCards.push(apiCardObj);
+        numberOfChangedCards += 1;
+        console.log('SWDestinyDB reponse has changed', numberOfChangedCards);
+      }
+    });
+  });
+  console.log(changedCards);
+  return changedCards;
+}
 
 router.get('/', (req, res) => {
   axios.get('https://swdestinydb.com/api/public/cards/')
@@ -15,18 +45,39 @@ router.get('/', (req, res) => {
 
 router.get('/sync', (req, res) => {
   axios.get('https://swdestinydb.com/api/public/cards/')
-    .then((response) => {
+    .then(async (response) => {
+      let changedCards = [];
+      let numberOfChangedCards = 0;
       /* eslint consistent-return: 0 */
       pool.connect((err, client, done) => {
         if (err) {
           return console.error('error fetching client from pool', err);
         }
-        // TODO:
-        // query for existing cards as to not have duplicates
-        // maybe look into comparing the json for changes?
-        response.data.forEach((swdDbCard) => {
+
+        client.query('SELECT data AS dbCard FROM card', [], (err, results) => {
+          if (err) {
+            return console.error('error running get modified cards query');
+          }
+          //console.log(response.data);
+          if (results.rowCount === 0) {
+            changedCards = response.data;
+          }
+
+          results.rows.forEach((row) => {
+            const apiCardObj = response.data.find((value) => {
+              return value.code === row.dbcard.code;
+            });
+
+            if (!deepEqual(apiCardObj, row.dbcard)) {
+              changedCards.push(apiCardObj);
+              numberOfChangedCards += 1;
+              console.log('SWDestinyDB reponse has changed', numberOfChangedCards);
+            }
+          });
+        });
+        console.log(changedCards);
+        changedCards.forEach((swdDbCard) => {
           client.query('INSERT INTO card (data) VALUES ($1)', [swdDbCard], (errQuery, result) => {
-            done(errQuery);
 
             if (errQuery) {
               return console.error('error running client query', errQuery);
