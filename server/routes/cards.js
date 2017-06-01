0eb2ab830970d1855232efa28e0e32c869d23f91
Aaron Lee <Worldedit05@gmail.com>
2017-06-01 02:02:13 -0500

    Refactoring to correct sync issue

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -1,37 +1,10 @@
-const router = require('express').Router();
 const axios = require('axios');
 const deepEqual = require('deep-equal');
+const Router = require('express-promise-router');
 
 const pool = require('../db.js');
 
-function getModifiedCards(responseData) {
-  let changedCards = [];
-  let numberOfChangedCards = 0;
-  pool.query('SELECT data AS dbCard FROM card', [], (err, results) => {
-    if (err) {
-      return console.error('error running get modified cards query');
-    }
-
-    if (results.rowCount === 0) {
-      changedCards = responseData;
-      return changedCards;
-    }
-
-    results.rows.forEach((row) => {
-      const apiCardObj = responseData.find((value) => {
-        return value.code === row.dbcard.code;
-      });
-
-      if (!deepEqual(apiCardObj, row.dbcard)) {
-        changedCards.push(apiCardObj);
-        numberOfChangedCards += 1;
-        console.log('SWDestinyDB reponse has changed', numberOfChangedCards);
-      }
-    });
-  });
-  console.log(changedCards);
-  return changedCards;
-}
+const router = new Router();
 
 router.get('/', (req, res) => {
   axios.get('https://swdestinydb.com/api/public/cards/')
@@ -43,56 +16,55 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/sync', (req, res) => {
-  axios.get('https://swdestinydb.com/api/public/cards/')
-    .then(async (response) => {
-      let changedCards = [];
-      let numberOfChangedCards = 0;
-      /* eslint consistent-return: 0 */
-      pool.connect((err, client, done) => {
-        if (err) {
-          return console.error('error fetching client from pool', err);
-        }
+router.get('/sync', async (req, res) => {
+  const response = await axios.get('https://swdestinydb.com/api/public/cards/').catch((ex) => {
+    console.error(ex);
+  });
+  let changedCards = [];
+  let numberOfChangedCards = 0;
+  let insertResults = '';
 
-        client.query('SELECT data AS dbCard FROM card', [], (err, results) => {
-          if (err) {
-            return console.error('error running get modified cards query');
-          }
-          //console.log(response.data);
-          if (results.rowCount === 0) {
-            changedCards = response.data;
-          }
+  /* eslint consistent-return: 0 */
+  const rows = await pool.connect((err, client, done) => {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
 
-          results.rows.forEach((row) => {
-            const apiCardObj = response.data.find((value) => {
-              return value.code === row.dbcard.code;
-            });
+    client.query('SELECT data AS dbCard FROM card', [], async (errSELECT, results) => {
+      done(errSELECT);
+      if (errSELECT) {
+        return console.error('error running get modified cards query');
+      }
 
-            if (!deepEqual(apiCardObj, row.dbcard)) {
-              changedCards.push(apiCardObj);
-              numberOfChangedCards += 1;
-              console.log('SWDestinyDB reponse has changed', numberOfChangedCards);
-            }
-          });
+      if (results.rowCount === 0) {
+        changedCards = response.data;
+      }
+
+      results.rows.forEach((row) => {
+        const apiCardObj = response.data.find((value) => {
+          return value.code === row.dbcard.code;
         });
-        console.log(changedCards);
-        changedCards.forEach((swdDbCard) => {
-          client.query('INSERT INTO card (data) VALUES ($1)', [swdDbCard], (errQuery, result) => {
 
-            if (errQuery) {
-              return console.error('error running client query', errQuery);
-            }
+        if (!deepEqual(apiCardObj, row.dbcard)) {
+          changedCards.push(apiCardObj);
+          numberOfChangedCards += 1;
+          console.log('SWDestinyDB reponse has changed', numberOfChangedCards);
+        }
+      });
 
-            return console.log(result);
-          });
+      await changedCards.forEach((swdDbCard) => {
+        client.query('INSERT INTO card (data) VALUES ($1)', [swdDbCard], (errQuery, result) => {
+          done(errQuery);
+
+          if (errQuery) {
+            return console.error('error running client query', errQuery);
+          }
+          insertResults += result.toString();
         });
       });
-    })
-    .catch((error) => {
-      console.log(error);
+      return res.json(insertResults);
     });
-
-  return res.json('Sync Complete!');
+  });
 });
 
 module.exports = router;
