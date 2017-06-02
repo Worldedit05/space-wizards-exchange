b7161ae0074870cd547d75bab9a877d3b51f34fb
Aaron Lee <Worldedit05@gmail.com>
2017-06-02 00:15:51 -0500

    Fixed the callback / promise issue - Thanks Brian!

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -17,53 +17,41 @@ router.get('/', (req, res) => {
 });
 
 router.get('/sync', async (req, res) => {
-  const response = await axios.get('https://swdestinydb.com/api/public/cards/').catch((ex) => {
-    console.error(ex);
-  });
-  let changedCards = [];
+  const response = await axios.get('https://swdestinydb.com/api/public/cards/');
+  let cardsToProcess = [];
   let numberOfChangedCards = 0;
-
+  let newResponse = '';
+  let changedCardsResults = '';
+  let cardsChanged;
   /* eslint consistent-return: 0 */
-  const rows = await pool.connect((err, client, done) => {
-    if (err) {
-      return console.error('error fetching client from pool', err);
-    }
 
-    client.query('SELECT data AS dbCard FROM card', [], async (errSELECT, results) => {
-      done(errSELECT);
-      if (errSELECT) {
-        return console.error('error running get modified cards query');
-      }
+  const result = await pool.query('SELECT data AS dbCard FROM card', []);
+  if (result.rowCount === 0) {
+    cardsToProcess = response.data;
+    cardsChanged = false;
+  }
 
-      if (results.rowCount === 0) {
-        changedCards = response.data;
-      }
-
-      results.rows.forEach((row) => {
-        const apiCardObj = response.data.find((value) => {
-          return value.code === row.dbcard.code;
-        });
-
-        if (!deepEqual(apiCardObj, row.dbcard)) {
-          changedCards.push(apiCardObj);
-          numberOfChangedCards += 1;
-          console.log('SWDestinyDB reponse has changed', numberOfChangedCards);
-        }
+  if (result.rowCount > 0) {
+    await result.rows.forEach((row) => {
+      const apiCardObj = response.data.find((value) => {
+        return value.code === row.dbcard.code;
       });
 
-      await changedCards.forEach((swdDbCard) => {
-        client.query('INSERT INTO card (data) VALUES ($1)', [swdDbCard], (errQuery, result) => {
-          done(errQuery);
-
-          if (errQuery) {
-            return console.error('error running client query', errQuery);
-          }
-          return console.log(result);
-        });
-      });
-      return res.send(rows);
+      if (!deepEqual(apiCardObj, row.dbcard)) {
+        cardsToProcess.push(apiCardObj);
+        numberOfChangedCards += 1;
+        cardsChanged = true;
+        console.log('SWDestinyDB reponse has changed', numberOfChangedCards);
+      }
     });
+  }
+
+  await cardsToProcess.forEach((swdDbCard) => {
+    const insertResults = pool.query('INSERT INTO card (data) VALUES ($1)', [swdDbCard]);
+    changedCardsResults += insertResults;
   });
+  newResponse = cardsChanged ? res.json(changedCardsResults + numberOfChangedCards) : res.json('No_cards_were_changed');
+  return newResponse;
 });
 
 module.exports = router;
