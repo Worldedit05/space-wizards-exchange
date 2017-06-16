4fdf2e8170f4feb4995e02daf77c4027ad500ada
Aaron Lee <Worldedit05@gmail.com>
2017-06-15 19:02:11 -0500

    Card sync tool now only updates if there is a card changegit status!

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -21,11 +21,10 @@ router.get('/sync', async (req, res) => {
   let cardsToProcess = [];
   let numberOfChangedCards = 0;
   let newResponse = '';
-  let changedCardsResults = '';
   let cardsChanged;
   /* eslint consistent-return: 0 */
 
-  const result = await pool.query('SELECT data AS dbCard FROM card', []);
+  const result = await pool.query('SELECT data  AS dbCard, swd_database_code AS cardCode FROM card', []);
   if (result.rowCount === 0) {
     cardsToProcess = response.data;
     cardsChanged = false;
@@ -34,23 +33,29 @@ router.get('/sync', async (req, res) => {
   if (result.rowCount > 0) {
     await result.rows.forEach((row) => {
       const apiCardObj = response.data.find((value) => {
-        return value.code === row.dbcard.code;
+        if (value.code === row.cardcode) {
+          console.log(`Card match found ${value.code}, ${row.cardcode}`);
+        }
+        return value.code === row.cardcode;
       });
 
       if (!deepEqual(apiCardObj, row.dbcard)) {
         cardsToProcess.push(apiCardObj);
         numberOfChangedCards += 1;
         cardsChanged = true;
-        console.log('SWDestinyDB reponse has changed', numberOfChangedCards);
       }
     });
+    console.log(`SWDestinyDB reponse has changed ${numberOfChangedCards} cards`);
   }
 
-  await cardsToProcess.forEach((swdDbCard) => {
-    const insertResults = pool.query('INSERT INTO card (data) VALUES ($1)', [swdDbCard]);
-    changedCardsResults += insertResults;
+  await cardsToProcess.forEach(async (swdDbCard) => {
+    console.log(swdDbCard.code);
+    const insertResults = await pool.query('INSERT INTO card (data, swd_database_code) VALUES ($1 , $2) ON CONFLICT (swd_database_code) DO UPDATE SET (data) = ($1);', [swdDbCard, swdDbCard.code]);
+    console.log(insertResults);
   });
-  newResponse = cardsChanged ? res.json(changedCardsResults + numberOfChangedCards) : res.json('No_cards_were_changed');
+
+  newResponse = cardsChanged ? res.json([{ numberOfChangedCards }, cardsToProcess]) : res.json('No_cards_were_changed');
+
   return newResponse;
 });
 
