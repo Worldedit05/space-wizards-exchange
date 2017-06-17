941f2939d036f41da02f1ec92b30c31a012dd52a
Aaron Lee <Worldedit05@gmail.com>
2017-06-17 10:32:59 -0500

    Removing MongoDB

--- server/app.js
+++ server/app.js
@@ -2,16 +2,6 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 
-// const mongoose = require('mongoose');
-// const Promise = require('bluebird');
-
-// const isProduction = process.env.NODE_ENV === 'production';
-//
-// const config = isProduction ? require('./config.prod.js') : require('./config.local.js');
-//
-// const mongoLocalConn = `mongodb://${config.mongo.hostname}:${config.mongo.port}/${config.mongo.database}`;
-// const mongoConnection = process.env.MONGODB_URI || mongoLocalConn;
-
 const cardRoutes = require('./routes/cards');
 const searchRoutes = require('./routes/search');
 
@@ -27,21 +17,5 @@ app.use('/search', searchRoutes);
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
-
-// mongoose.Promise = Promise;
-//
-// mongoose.connect(mongoConnection, () => {
-//   console.log(`\nMongo connection successful\nURI= ${mongoConnection}`);
-// });
-//
-// const db = mongoose.connection;
-
-// db.on('error', (error) => {
-//   console.log(`Mongoose Error: ${error}`);
-// });
-//
-// db.once('open', () => {
-//   console.log('Mongoose is up and running!');
-// });
 
 module.exports = app;
