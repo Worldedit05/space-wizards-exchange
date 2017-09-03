9cdd57fbe7b56d7944ed061703d4b9242e164097
Aaron Lee <Worldedit05@gmail.com>
2017-09-03 17:57:23 -0500

    Cleaning up errors and refactoring for a saveCard function

--- server/models/database.js
+++ server/models/database.js
@@ -2,6 +2,6 @@
 /*
 create table account(id uuid PRIMARY KEY DEFAULT uuid_generate_v4(), email varchar(75) NOT null UNIQUE, first_name varchar(50), last_name varchar(50), username varchar(75) NOT NULL, city varchar(100), country varchar(2), rating int, trade_notes varchar(500));
 create unique index users_unique_lower_email_idx on account (lower(email));
-create table card(id uuid PRIMARY KEY DEFAULT uuid_generate_v4(), swd_database_code int UNIQUE, data jsonb);
+create table card(id uuid PRIMARY KEY DEFAULT uuid_generate_v4(), swd_code varchar(8) UNIQUE, data jsonb);
 create table user_card_list(user_id uuid REFERENCES account(id), card_id uuid REFERENCES card(id), want boolean NOT null DEFAULT false, willing_to_trade boolean NOT null DEFAULT false);
 */
--- server/routes/cards.js
+++ server/routes/cards.js
@@ -19,20 +19,23 @@ router.get('/', (req, res) => {
 router.get('/sync', async (req, res) => {
   const response = await axios.get('https://swdestinydb.com/api/public/cards/');
   const insertedCardsList = [];
-  const insertNewCardResults = '';
-  const insertChangedCardResults = '';
+  let insertNewCardResults;
+  let insertChangedCardResults;
   let numberOfChangedCards = 0;
   let numberofNewCards = 0;
-  let newResponse = '';
   let cardsInserted;
+
+  const saveCard = async (card) => {
+    insertNewCardResults = await db.query('INSERT INTO card (data, swd_code) VALUES ($1 , $2) ON CONFLICT (swd_code) DO UPDATE SET (data) = ($1);', [card, card.code]);
+  };
 
   /* eslint-disable */
   for (const card of response.data) {
-    const dbCard = await db.query('SELECT data AS dbCard FROM card WHERE swd_database_code = ($1)', [card.code]);
+    const dbCard = await db.query('SELECT data AS dbCard FROM card WHERE swd_code = ($1)', [card.code]);
 
     if (dbCard.rowCount === 0) {
       try {
-        insertNewCardResults = await db.query('INSERT INTO card (data, swd_database_code) VALUES ($1 , $2) ON CONFLICT (swd_database_code) DO UPDATE SET (data) = ($1);', [card, card.code]);
+        saveCard(card);
       }
       catch (err) {
         console.log(err);
@@ -48,7 +51,7 @@ router.get('/sync', async (req, res) => {
 
       if (!deepEqual(card, dbCard.rows[0].dbcard)) {
         try {
-          insertChangedCardResults = await db.query('INSERT INTO card (data, swd_database_code) VALUES ($1 , $2) ON CONFLICT (swd_database_code) DO UPDATE SET (data) = ($1);', [card, card.code]);
+          saveCard(card);
         }
         catch (err) {
           console.log(err);
@@ -66,7 +69,7 @@ router.get('/sync', async (req, res) => {
 
   console.log(`SWDestinyDB reponse has changed ${numberOfChangedCards} cards`);
 
-  newResponse = cardsInserted ? res.json([{ numberofNewCards }, { numberOfChangedCards }, insertedCardsList]) : res.json('No_cards_were_changed');
+  const newResponse = cardsInserted ? res.json({ numberofNewCards, numberOfChangedCards, insertedCardsList }) : res.json('No_cards_were_changed');
   return newResponse;
 });
 
